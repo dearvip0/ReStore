9c15ead0fef01792fb7e8f5babb6ed5ac427c114
thuongvb <baothuong9608@gmail.com>
2021-10-28 15:50:08 +0700

    End of react basic and materialUI

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,35 @@
+import { Container, CssBaseline } from "@mui/material";
+import Catalog from "../../features/catalog/Catalog";
+import Header from "./Header";
+
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useState } from "react";
+
+function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? "dark" : "light";
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+      background: {
+        default: paletteType === "light" ? "#eaeaea" : "rgba(101,108,133,0.8)",
+      },
+    },
+  });
+
+  const handleThemeChange = () => {
+    setDarkMode(!darkMode);
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
+      <Container>
+        <Catalog />
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default App;
